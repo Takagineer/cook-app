989cf0330cf32ec8ad87950c2157d67b229f98da
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-11-09 17:02:58 +0900

    ブランチへのpush

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>テスト</h1>
+      <h3>ひとまずデプロイ</h3>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
